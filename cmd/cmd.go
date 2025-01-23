aa01a2b30050547d81f53b51a231c6b37d70393a
Erik <erikzhou7@gmail.com>
2025-01-22 22:45:08 -0500

    Added ssh support (had to downgrade wish due to bug in the library)

--- cmd/cmd.go
+++ cmd/cmd.go
@@ -1,18 +1,35 @@
 package cmd
 
 import (
+	"context"
 	"fmt"
+	"log"
 	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/ssh"
+	"github.com/charmbracelet/wish"
+	"github.com/charmbracelet/wish/activeterm"
+	"github.com/charmbracelet/wish/bubbletea"
+	lm "github.com/charmbracelet/wish/logging"
 	"github.com/muesli/termenv"
 	"github.com/spf13/cobra"
 	"golang.org/x/term"
 )
 
 var (
-	Version = "dev"
-	RootCmd = &cobra.Command{
+	host           = "localhost"
+	port           = 2222
+	privateKeyPath string
+)
+
+var (
+	Version       = "dev"
+	sshServerFlag bool
+	RootCmd       = &cobra.Command{
 		Use:  "TermTyper",
 		Long: "TermTyper - Terminal Typing Test",
 		RunE: func(cmd *cobra.Command, args []string) error {
@@ -35,4 +52,82 @@ var (
 			return err
 		},
 	}
+	serveCmd = &cobra.Command{
+		Use:  "serve",
+		Long: "Serve as an SSH server",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			s, err := wish.NewServer(
+				wish.WithAddress(fmt.Sprintf("%s:%d", host, port)),
+				wish.WithHostKeyPath(privateKeyPath),
+				wish.WithMiddleware(
+					bubbletea.Middleware(teaHandler),
+					activeterm.Middleware(),
+					lm.Middleware(),
+				),
+			)
+
+			if err != nil {
+				return err
+			}
+
+			done := make(chan os.Signal, 1)
+			signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+
+			log.Printf("Starting server on %s:%d", host, port)
+			go func() {
+				if err := s.ListenAndServe(); err != nil {
+					log.Fatalln(err)
+				}
+			}()
+
+			<-done
+
+			log.Printf("Stopping SSH server on %s:%d", host, port)
+			ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+			defer func() { cancel() }()
+			if err := s.Shutdown(ctx); err != nil {
+				return err
+			}
+
+			return nil
+		},
+	}
 )
+
+func init() {
+	RootCmd.PersistentFlags().BoolVar(&sshServerFlag, "ssh-server", false, "Serve as an SSH server")
+	serveCmd.Flags().StringVarP(&privateKeyPath, "key", "k", "id_rsa", "path to the server key")
+	serveCmd.Flags().StringVarP(&host, "host", "", "localhost", "address to serve on")
+	serveCmd.Flags().IntVarP(&port, "port", "p", 2229, "port to serve on")
+	RootCmd.AddCommand(serveCmd)
+}
+
+func teaHandler(s ssh.Session) (tea.Model, []tea.ProgramOption) {
+	// This should never fail, as we are using the activeterm middleware.
+	pty, _, _ := s.Pty()
+
+	// When running a Bubble Tea app over SSH, you shouldn't use the default
+	// lipgloss.NewStyle function.
+	// That function will use the color profile from the os.Stdin, which is the
+	// server, not the client.
+	// We provide a MakeRenderer function in the bubbletea middleware package,
+	// so you can easily get the correct renderer for the current session, and
+	// use it to create the styles.
+	// The recommended way to use these styles is to then pass them down to
+	// your Bubble Tea model.
+	pty, _, active := s.Pty()
+
+	if !active {
+		wish.Fatal(s, fmt.Errorf("not a tty"))
+		return nil, nil
+	}
+
+	m := initModel(
+		termenv.ANSI256,
+		termenv.ANSIWhite,
+		pty.Window.Width,
+		pty.Window.Height,
+	)
+
+	return m, []tea.ProgramOption{tea.WithAltScreen()}
+}
--- go.mod
+++ go.mod
@@ -6,17 +6,27 @@ require (
 	github.com/charmbracelet/bubbles v0.20.0
 	github.com/charmbracelet/bubbletea v1.2.4
 	github.com/charmbracelet/lipgloss v1.0.0
+	github.com/charmbracelet/ssh v0.0.0-20241211182756-4fe22b0f1b7c
+	github.com/charmbracelet/wish v1.3.0
 	github.com/muesli/reflow v0.3.0
-	github.com/muesli/termenv v0.15.2
+	github.com/muesli/termenv v0.15.3-0.20240509142007-81b8f94111d5
 	github.com/spf13/cobra v1.8.1
-	golang.org/x/term v0.26.0
+	golang.org/x/term v0.28.0
 )
 
 require (
+	github.com/anmitsu/go-shlex v0.0.0-20200514113438-38f4b401e2be // indirect
 	github.com/aymanbagabas/go-osc52/v2 v2.0.1 // indirect
-	github.com/charmbracelet/x/ansi v0.5.2 // indirect
+	github.com/charmbracelet/keygen v0.5.1 // indirect
+	github.com/charmbracelet/log v0.4.0 // indirect
+	github.com/charmbracelet/x/ansi v0.7.0 // indirect
+	github.com/charmbracelet/x/conpty v0.1.0 // indirect
+	github.com/charmbracelet/x/errors v0.0.0-20250122132629-a969ddeb820d // indirect
 	github.com/charmbracelet/x/term v0.2.1 // indirect
+	github.com/charmbracelet/x/termios v0.1.0 // indirect
+	github.com/creack/pty v1.1.24 // indirect
 	github.com/erikgeiser/coninput v0.0.0-20211004153227-1c3628e74d0f // indirect
+	github.com/go-logfmt/logfmt v0.6.0 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
@@ -26,7 +36,9 @@ require (
 	github.com/muesli/cancelreader v0.2.2 // indirect
 	github.com/rivo/uniseg v0.4.7 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
-	golang.org/x/sync v0.9.0 // indirect
-	golang.org/x/sys v0.27.0 // indirect
-	golang.org/x/text v0.20.0 // indirect
+	golang.org/x/crypto v0.32.0 // indirect
+	golang.org/x/exp v0.0.0-20250106191152-7588d65b2ba8 // indirect
+	golang.org/x/sync v0.10.0 // indirect
+	golang.org/x/sys v0.29.0 // indirect
+	golang.org/x/text v0.21.0 // indirect
 )

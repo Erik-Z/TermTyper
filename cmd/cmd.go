bc688a086e28b6e392cd7a509935f37742255a86
Erik <erikzhou7@gmail.com>
2025-01-24 17:49:07 -0500

    removed useless code/comments

--- cmd/cmd.go
+++ cmd/cmd.go
@@ -22,7 +22,7 @@ import (
 
 var (
 	host           = "localhost"
-	port           = 2222
+	port           = 22222
 	privateKeyPath string
 )
 
@@ -98,29 +98,12 @@ func init() {
 	RootCmd.PersistentFlags().BoolVar(&sshServerFlag, "ssh-server", false, "Serve as an SSH server")
 	serveCmd.Flags().StringVarP(&privateKeyPath, "key", "k", "id_rsa", "path to the server key")
 	serveCmd.Flags().StringVarP(&host, "host", "", "localhost", "address to serve on")
-	serveCmd.Flags().IntVarP(&port, "port", "p", 2229, "port to serve on")
+	serveCmd.Flags().IntVarP(&port, "port", "p", port, "port to serve on")
 	RootCmd.AddCommand(serveCmd)
 }
 
 func teaHandler(s ssh.Session) (tea.Model, []tea.ProgramOption) {
-	// This should never fail, as we are using the activeterm middleware.
 	pty, _, _ := s.Pty()
-
-	// When running a Bubble Tea app over SSH, you shouldn't use the default
-	// lipgloss.NewStyle function.
-	// That function will use the color profile from the os.Stdin, which is the
-	// server, not the client.
-	// We provide a MakeRenderer function in the bubbletea middleware package,
-	// so you can easily get the correct renderer for the current session, and
-	// use it to create the styles.
-	// The recommended way to use these styles is to then pass them down to
-	// your Bubble Tea model.
-	pty, _, active := s.Pty()
-
-	if !active {
-		wish.Fatal(s, fmt.Errorf("not a tty"))
-		return nil, nil
-	}
 
 	m := initModel(
 		termenv.ANSI256,
